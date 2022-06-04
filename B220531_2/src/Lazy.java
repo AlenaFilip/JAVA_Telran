357433b6547b9148ee733c15c32a04d6f8aea3bd
AlenaFilip <villager.bel.lena@gmail.com>
2022-06-04 15:47:28 +0200

    test request2

--- B220531_2/src/Lazy.java
+++ B220531_2/src/Lazy.java
@@ -3,4 +3,9 @@ public class Lazy extends Employee {
     public Lazy(String name, double salary) {
         super(name, salary);
     }
+
+    @Override
+    public void work() {
+
+    }
 }
---
+++ B220531_2/src/Tmp.java
@@ -0,0 +1,17 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class Tmp {
+
+    public static void aaa() {
+        List<Integer> list = new ArrayList<>();
+        list.add(1);
+        ArrayList<Integer> alist = new ArrayList<>();
+        alist.add(5);
+        alist.add(888);
+        alist.add(-5);
+        alist.add(45);
+        System.out.println(alist);
+    }
+
+}
