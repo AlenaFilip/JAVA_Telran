a5a183a6c41b072ba9320e7215f4b9a016dccb81
AlenaFilip <villager.bel.lena@gmail.com>
2022-06-28 20:54:25 +0200

    Algorithm hw 22-06-28

---
+++ A220628_dinamicProgramming/src/Main.java
@@ -0,0 +1,68 @@
+/*  Результат выполнения на 40:
+обычная рекусия
+ */
+import java.util.Arrays;
+
+public class Main {
+    public static void main(String[] args) {
+
+        int num = 50;
+        int[] arr = new int[num + 1];
+        for (int i = 2; i <= num; i++) {
+            arr[i] = -1;
+        }
+//        System.out.println(Arrays.toString(arr));
+
+        long time1 = System.currentTimeMillis();
+        System.out.println(fibonacciRecursion(num));
+        long time2 = System.currentTimeMillis();
+        System.out.println("  - time = "+(time2-time1));
+
+        time1 = System.currentTimeMillis();
+        System.out.println(fibonacciDpMemoization(num, arr));
+        time2 = System.currentTimeMillis();
+        System.out.println("  - time = "+(time2-time1));
+
+        time1 = System.currentTimeMillis();
+        System.out.println(fibonacciDpTabulation(num));
+        time2 = System.currentTimeMillis();
+        System.out.println("  - time = "+(time2-time1));
+
+    }
+
+    private static int fibonacciRecursion(int num) {
+        if (num == 1) return 1;
+        else if (num == 0) return 0;
+
+        return fibonacciRecursion(num - 1) + fibonacciRecursion(num - 2);
+    }
+
+    private static int fibonacciDpMemoization(int num, int[] arr) {
+        if (num <= 2) {
+            return 1;
+        }
+        if(arr[num] != -1) {
+            return arr[num];
+        }
+
+        arr[num] = fibonacciDpMemoization(num - 1, arr) + fibonacciDpMemoization(num - 2, arr);
+        return arr[num];
+    }
+
+    private static int fibonacciDpTabulation(int num) {
+        int[] arr = new int[num + 1];
+        arr[0] = 0;
+        arr[1] = 1;
+
+        int res = 0;
+
+        for (int index = 2; index <= num; index++) {
+            arr[index] = arr[index - 2] + arr[index - 1];
+
+            if (index == num) {
+                res = arr[index];
+            }
+        }
+        return res;
+    }
+}
---
+++ A220628_megreSort/src/Main.java
@@ -0,0 +1,66 @@
+import java.util.Arrays;
+
+public class Main {
+
+    public static void main(String args[]) {
+        /*
+        mergeSort(arr) - method that sorts arr[] using marge()
+        merge(arr, leftArr, rightArr) - merge two subarrays of arr[]
+         */
+
+        int[] arr = {15, 21, 13, 5, 10, 7};
+        System.out.println(Arrays.toString(arr));
+        mergeSort(arr);
+        System.out.println(Arrays.toString(arr));
+    }
+
+    private static void mergeSort(int[] arr) {
+        int lengthArr = arr.length; // длинна массивы
+        if(lengthArr == 1) { // условие выхода
+            return;
+        }
+
+        int mid = lengthArr / 2;
+        int[] leftArr = new int[mid]; // левый подмассив
+        int[] rightArr = new int[lengthArr - mid]; // правый подмассив
+
+        // копируем элементы из массива в подмассивы
+        for (int i = 0; i < mid; i++) {
+            leftArr[i] = arr[i];
+        }
+
+        for (int j = 0; j < lengthArr - mid; j++) {
+            rightArr[j] = arr[j + mid];
+        }
+
+        mergeSort(leftArr);
+        mergeSort(rightArr);
+
+        // когда мы достигли базового условия
+        merge(arr, leftArr, rightArr);
+    }
+
+    private static void merge(int[] arr, int[] leftArr, int[] rightArr) {
+        int indL = 0;
+        int indR = 0;
+        int lenL = leftArr.length;
+        int lenR = rightArr.length;
+        while(indL<lenL && indR<lenR){
+            if(leftArr[indL]<rightArr[indR]){
+                arr[indL+indR] = leftArr[indL];
+                indL++;
+            } else {
+                arr[indL+indR] = rightArr[indR];
+                indR++;
+            }
+        }
+        while (indL<lenL) {
+            arr[indL+indR] = leftArr[indL];
+            indL++;
+        }
+        while (indR<lenR) {
+            arr[indL+indR] = rightArr[indR];
+            indR++;
+        }
+    }
+}
